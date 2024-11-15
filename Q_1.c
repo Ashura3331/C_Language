65d2912a96e1f62cab4dd36253c238fad6ba57da
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-15 22:12:01 +0600

    new files

---
+++ Q_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    char name[50] = "Ahsan Habib";
+    char dateOfBirth[15] = "01 January 1990";
+    char mobileNumber[15] = "+8801723098771";
+    
+
+
+    return 0;
+}
