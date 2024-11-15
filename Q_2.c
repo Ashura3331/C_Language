b0e262cb7e831bc41cdc31d8e4d9c57d605f535b
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-15 22:41:57 +0600

    new file add

---
+++ Q_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+
+    char gradeMark[300] = "‟Recently I heard that you've achieve 95% marks in your exam.\n This is brilliant!\n I wish you'll shine in your life!\t Good luck with all the barriers(∧)in your life.”";
+
+    printf("%s", gradeMark);
+
+
+    return 0;
+}
