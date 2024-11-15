da312720026605435ce2bea5a1f362ffef3001c7
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-15 22:31:38 +0600

    new files

--- Q_1.c
+++ Q_1.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main() {
     char name[50] = "Ahsan Habib";
-    char dateOfBirth[15] = "01 January 1990";
+    char dateOfBirth[25] = "01 January 1990";
     char mobileNumber[15] = "+8801723098771";
-    
+
+    printf("Name:  %s\n", name);
+    printf("Date of Birth:  %s\n", dateOfBirth);
+    printf("Mobile Number:  %s\n", mobileNumber);
 
 
     return 0;
