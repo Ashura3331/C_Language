ac05bc20b2513419d90e1fa7452ec4c60707e48b
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-25 00:00:44 +0600

    new data

--- Classes_Queation/2D_array.c
+++ Classes_Queation/2D_array.c
@@ -21,13 +21,28 @@ int findLargest(int array[ROWS][COLS]) {
 }
 
 // Function to multiply a 2D array by a scalar
-int multiplyArray(int arr[ROWS][COLS], int multiplier, int result[ROWS][COLS]) {
+void multiplyArray(int array[ROWS][COLS], int multiplier, int result[ROWS][COLS]) {
     for (int i = 0; i < ROWS; i++)
     {
-        for (int j = 0; j < COLS; i++)
+        for (int j = 0; j < COLS; j++)
         {
             result[i][j] = array[i][j] * multiplier;
         }
+        
+    }
+    
+}
+
+
+// Function to print a 2D array
+void printArray (int array[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            printf("%d\t", array[i][j]);
+        }
+        printf("\n");
         
     }
     
@@ -38,7 +53,27 @@ int multiplyArray(int arr[ROWS][COLS], int multiplier, int result[ROWS][COLS]) {
 
 int main() {
 
+    int array1[ROWS][COLS] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
 
+    int array2[ROWS][COLS] {
+        {9, 8, 7},
+        {6, 5, 4}, 
+        {3, 2, 1}
+    };
+
+    int result[ROWS][COLS];
+
+    int largest = findLargest(array1);
+    printf("The larget number in the array is: %d\n\n", largest);
+
+    multiplyArray(array2, largest, result);
+
+    printf("The result array after multiplicaton is : \n");
+    printArray(result);
 
     return 0;
 }
