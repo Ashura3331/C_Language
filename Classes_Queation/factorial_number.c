8d0ce972d5baf2aab14bdfdb3c3a3f0a69f53bd9
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-24 23:14:13 +0600

    new data

--- Classes_Queation/factorial_number.c
+++ Classes_Queation/factorial_number.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
+int factorialRecursion (int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorialRecursion(n -1);
+}
+
+int factorialIteration (int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+    
+}
 
 
 
 
-
-int main() {}
+int main() {
+    
+}
