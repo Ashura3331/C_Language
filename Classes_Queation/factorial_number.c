8e9d15c0e57afc572bc1bb3315bbd6976d859e9d
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-24 23:10:34 +0600

    new file practice

--- Classes_Queation/factorial_number.c
+++ Classes_Queation/factorial_number.c
@@ -1,41 +1,8 @@
-#include<stdio.h>
-
-
- int factorialRecursion (int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        return n * factorialRecursion(n - 1);
-    }
-
-    int factorialIteration(int n) {
-        int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
-
-
-int main() {
-
-    int num;
-    printf("Enter a number to find its factorial: ");
-    scanf("%d", &num);
-
-    if (num < 0) {
-        printf("Factorial is not defined for negative number.\n");
-    }else {
-        int resultRecursion = factorialRecursion(num);
-        printf("Facrial (using recursion): %d\n", resultRecursion);
-
-        // Factorial using Iteraion
-        int resultIteration = factorialIteration(num);
-        printf("Factorial (using iteration): %d\n", resultIteration);
-    }
-   
+#include <stdio.h>
 
 
 
-    return 0;
-}
+
+
+
+int main() {}
