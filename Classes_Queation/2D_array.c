53347af287b71ff94c1de5adce9b86b864e5e33d
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-24 23:45:34 +0600

    new data

--- Classes_Queation/2D_array.c
+++ Classes_Queation/2D_array.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+#define ROWS 3
+#define COLS 3
 
+int findLargest(int array[ROWS][COLS]) {
+    int largest = array[0][0];
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            if (array[i][j] > largest) {
+                largest = array[i][j];
+            }
+        }
+        
+    }
+    return largest;
+}
 
 
 
