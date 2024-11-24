7e82429dc50369229bacc7c27420809bb2a65b8a
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-24 23:48:57 +0600

    new data again

--- Classes_Queation/2D_array.c
+++ Classes_Queation/2D_array.c
@@ -3,6 +3,7 @@
 #define ROWS 3
 #define COLS 3
 
+// Function to find the largest number in a 2D array
 int findLargest(int array[ROWS][COLS]) {
     int largest = array[0][0];
 
@@ -19,7 +20,18 @@ int findLargest(int array[ROWS][COLS]) {
     return largest;
 }
 
-
+// Function to multiply a 2D array by a scalar
+int multiplyArray(int arr[ROWS][COLS], int multiplier, int result[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; i++)
+        {
+            result[i][j] = array[i][j] * multiplier;
+        }
+        
+    }
+    
+}
 
 
 
