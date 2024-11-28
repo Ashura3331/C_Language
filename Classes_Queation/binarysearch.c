74948dc040db07c398e2502052b4a006e8bd7723
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-29 00:31:10 +0600

    new data

--- Classes_Queation/binarysearch.c
+++ Classes_Queation/binarysearch.c
@@ -1,10 +1,38 @@
 
 #include <stdio.h>
 
+int binarySearch(int array[], int size, int target) {
+    int left = 0, right = size - 1;
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if (array[mid] == target) {
+            return mid;
+        }
+        if (array[mid] <  target) {
+            left = mid + 1;
+        }else {
+            right = mid - 1;
+        }
+    }
 
+    return -1;
+}
 
 int main() {
 
+int array[] = {1, 3, 5, 7, 9, 13, 15, 19, 27, 33, 99};
+int size = sizeof(array) / sizeof(array[0]);
+int num;
+
+printf("Enter your number: ");
+scanf("%d", &num);
+
+int result = binarySearch(array, size, num);
+if (resut != -1) {
+    printf("Element found at index: %d\n", result);
+}else {
+    printf("Element not found\n");
+}
 
 
 
