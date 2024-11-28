befcc42afaa1e1276115fb20503d0b3d159a4c8f
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-28 23:58:29 +0600

    new changes

--- Classes_Queation/binary_search.c
+++ Classes_Queation/binary_search.c
@@ -24,6 +24,19 @@ int binarySearch(int array[], int size, int target) {
 
 int main() {
 
+    int array[] = {2, 3, 7, 9, 13, 15, 21, 31, 33, 99};
+    int size = sizeof(array) / sizeof(array[0]);
+    int target;
+
+    printf("Enter the number of search: ");
+    scanf("%d\n", &target);
+
+    int result = binarySearch(array, size, target);
+    if (result != -1) {
+        printf("Element found at index: %d\n", result);
+    }else {
+        printf("Element not found\n");
+    }
 
     return 0;
 }
