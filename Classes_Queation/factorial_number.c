4fd7faec0e4410c3b3c231d13db29e5560e89962
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-28 23:26:10 +0600

    all files

--- Classes_Queation/factorial_number.c
+++ Classes_Queation/factorial_number.c
@@ -1,41 +1,41 @@
 #include <stdio.h>
 
-// int factorialRecursion (int n) {
-//     if(n == 0 || n == 1) {
-//         return 1;
-//     }
-//     return n * factorialRecursion(n-1);
-// }
+int factorialRecursion (int n) {
+    if(n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorialRecursion(n-1);
+}
 
-// int factorialIteration (int n) {
-//     int result = 1;
-//     for (int i = 1; i <= n; i++)
-// {
-//         result *= i;
-//     }
-//     return result;
+int factorialIteration (int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+{
+        result *= i;
+    }
+    return result;
     
-// }
+}
 
 
 
 
 int main() {
 
-    // int num;
+    int num;
 
-    // printf("Enter your number to find Factorial number: ");
-    // scanf("%d", &num);
+    printf("Enter your number to find Factorial number: ");
+    scanf("%d", &num);
 
-    // if (num < 0) {
-    //     printf("The negative number don't have a factorial number.");
-    // }else {
-    //     int resultRecursion = factorialIteration(num);
-    //     printf("Recursion (using recursion) : %d\n", resultRecursion);
+    if (num < 0) {
+        printf("The negative number don't have a factorial number.");
+    }else {
+        int resultRecursion = factorialIteration(num);
+        printf("Recursion (using recursion) : %d\n", resultRecursion);
 
-    //     int resultIteration = factorialIteration(num);
-    //     printf("Iteration (using iteration): %d\n", resultIteration);
-    // }
+        int resultIteration = factorialIteration(num);
+        printf("Iteration (using iteration): %d\n", resultIteration);
+    }
 
 
 
